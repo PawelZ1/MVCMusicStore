10bf9cf5aa6d2355f426be7ce30f7ed08515f915
PawelZ1 <pawel.zassowski@gmail.com>
2018-03-14 09:03:10 +0100

    initial VS files

---
+++ MVCMusicStore.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCMusicStore.Client.MvcApplication" Language="C#" %>
---
+++ MVCMusicStore.Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMusicStore.Client.Startup))]
+namespace MVCMusicStore.Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
